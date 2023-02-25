00019e602b5add3254df8f0592311d60423aca06
B0mbastic <kovbasin@gmai.com>
2023-02-26 02:50:34 +0800

    adding Snapkit constraints

--- Homework_Autolayout/ViewController.swift
+++ Homework_Autolayout/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
@@ -40,7 +41,7 @@ class ViewController: UIViewController {
     // массив констрейнтов для портретной ориентации
     private var narrowConstraints: [NSLayoutConstraint] = []
     
-    //  массив констрейнтов одинаковых для обеих ориентаций (не стал задавать)
+    // массив констрейнтов одинаковых для обеих ориентаций (не стал задавать)
     //private var commonConstraints: [NSLayoutConstraint] = []
     
     override func viewDidLoad() {
@@ -48,62 +49,67 @@ class ViewController: UIViewController {
         setupViews()
     }
     
+    private func addConstraints(orientation: String) -> Void {
+        switch orientation {
+        case "portrait":
+            FirstLabel.snp.remakeConstraints { make in
+                make.leading.equalToSuperview().inset(16)
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
+            }
+            SecondLabel.snp.remakeConstraints { make in
+                make.leading.equalTo(FirstLabel.snp.trailing).offset(16)
+                make.trailing.equalToSuperview().inset(16)
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(30)
+            }
+            ImageView.snp.remakeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.top.lessThanOrEqualTo(FirstLabel.snp.bottom).offset(10)
+                make.bottom.equalTo(view.snp.centerY).offset(-0.1*view.frame.height)
+                make.width.equalTo(ImageView.snp.height)
+            }
+        case "landscape":
+            FirstLabel.snp.remakeConstraints { make in
+                make.centerY.equalToSuperview()
+                make.leading.equalToSuperview().inset(20)
+            }
+            SecondLabel.snp.remakeConstraints { make in
+                make.centerY.equalToSuperview()
+                make.leading.equalTo(FirstLabel.snp.trailing).offset(20)
+            }
+            ImageView.snp.remakeConstraints { make in
+                make.centerY.equalToSuperview()
+                make.leading.equalTo(SecondLabel.snp.trailing).offset(20)
+                make.trailing.equalToSuperview().inset(20)
+                make.height.equalTo(ImageView.snp.width)
+            }
+        default:
+            print ("errror!")
+        }
+    }
+    
     private func setupViews() {
         view.addSubview(FirstLabel)
         view.addSubview(SecondLabel)
         view.addSubview(ImageView)
         
-        narrowConstraints = [
-            FirstLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            FirstLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-        
-            SecondLabel.leadingAnchor.constraint(equalTo: FirstLabel.trailingAnchor, constant: 8),
-            SecondLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            SecondLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            
-            ImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            ImageView.topAnchor.constraint(lessThanOrEqualTo: FirstLabel.bottomAnchor, constant: 10),
-            ImageView.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -view.frame.height/10),
-            ImageView.widthAnchor.constraint(equalTo: ImageView.heightAnchor, multiplier: 1)
-        ]
-        
-        wideConstraints = [
-            FirstLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            FirstLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        
-            SecondLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            SecondLabel.leadingAnchor.constraint(equalTo: FirstLabel.trailingAnchor, constant: 20),
-            
-            ImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            ImageView.leadingAnchor.constraint(equalTo: SecondLabel.trailingAnchor, constant: 20),
-            ImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            //ImageView.widthAnchor.constraint(equalToConstant: 50),
-            ImageView.heightAnchor.constraint(equalTo: ImageView.widthAnchor, multiplier: 1)
-        ]
-        // активируем общие констрейнты если надо (не стал активировать)
-        //NSLayoutConstraint.activate(commonConstraints)
-        
         if view.frame.width > view.frame.height {
             // проверка на альбомный режим
-            NSLayoutConstraint.activate(wideConstraints)
+            addConstraints(orientation: "landscape")
         } else {
             // проверка на портретный режим
-            NSLayoutConstraint.activate(narrowConstraints)
+            addConstraints(orientation: "portrait")
         }
         // Do any additional setup after loading the view.
     }
-
     override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
         super.willTransition(to: newCollection, with: coordinator)
             let isLandscape = UIDevice.current.orientation.isLandscape
             if isLandscape {
                 // поворачиваем горизонтально и меняем констрейнты
-                NSLayoutConstraint.deactivate(self.narrowConstraints)
-                NSLayoutConstraint.activate(self.wideConstraints)
+                addConstraints(orientation: "landscape")
             } else {
                 // поворачиваем вертикально и меняем констрейнты
-                NSLayoutConstraint.deactivate(self.wideConstraints)
-                NSLayoutConstraint.activate(self.narrowConstraints)
+                addConstraints(orientation: "portrait")
             }
         }
 }
